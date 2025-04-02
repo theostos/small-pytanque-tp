e92b7addcc05172132e0073e3a60070b51be5d85
Théo <theo@jumbomana.fr>
2025-04-02 10:07:06 +0200

    clean premises, add proof-wala

--- utils.py
+++ utils.py
@@ -8,7 +8,7 @@ import json
 
 import requests
 
-from client import ProofAssistantClientAPI
+from client import ProofAssistantClientAPI, goals_to_str
 
 logger = logging.getLogger()
 logger.setLevel(logging.CRITICAL)
@@ -18,7 +18,7 @@ Proof = List[Tactic]
 Score = float
 
 
-def generate_tactics(num_candidates: int, goals=[]
+def generate_tactics_dummy(num_candidates: int, goals=[]
 ) -> Tuple[List[str], List[float]]:
     hyp_names = []
     for goal in goals:
@@ -35,46 +35,66 @@ def generate_tactics(num_candidates: int, goals=[]
         random.uniform(0, 1) for _ in range(num_candidates)
     ]
 
-def beam_search(client: ProofAssistantClientAPI, section: str, index_thm: int, max_depth: int, beam_size: int = 5, timeout: float = 10.) -> Optional[List[str]]:
+def beam_search(generate_tactics, client: ProofAssistantClientAPI, section: str, index_thm: int, max_depth: int, beam_size: int = 5, timeout: float = 10.) -> Optional[List[str]]:
     start_time = time.time()
 
     # Each beam item is a tuple: (state, proof_so_far)
     state, goals = client.start_thm(section, index_thm)
-    beam: List[Tuple[dict, List[str]]] = [(state, goals, [])]
+    thm =  client.show_thm(section, index_thm)
 
+    theorems = []
+    definitions = []
+
+    for premise in thm['premises']:
+        if premise.startswith('Definition '):
+            premise = premise.replace('Definition ', '').strip()
+            definitions.append(premise)
+        elif premise.startswith('Notation '):
+            premise = premise.replace('Notation ', '').strip()
+            definitions.append(premise)
+        else:
+            theorems.append(premise)
+    
+    beam: List[Tuple[float, dict, List[str], List[str]]] = [(0., state, goals, [], [])]
+    visited = set()
     for _ in range(max_depth):
         if time.time() - start_time > timeout:
             return None
 
-        next_beam: List[Tuple[float, dict, dict, List[str]]] = []
-
-        for state, goals, proof_so_far in beam:
+        next_beam: List[Tuple[float, dict, dict, List[str], List[str]]] = []
+        for score, state, goals, steps, incorrect_steps in beam:
             try:
-                tactics, scores = generate_tactics(beam_size, goals)
-
-                for tac, score in zip(tactics, scores):
+                tactics, new_scores = generate_tactics(beam_size, goals, steps=steps, incorrect_steps=incorrect_steps, theorems=theorems, definitions=definitions)
+                for tac, new_score in zip(tactics, new_scores):
                     if time.time() - start_time > timeout:
                         return None
 
                     try:
                         new_state, new_goals = client.run_tac(state, tac)
-                        new_proof = proof_so_far + [tac]
+                        new_goals_pp = goals_to_str(new_goals)
+                        if new_goals_pp in visited:
+                            continue
+
+                        visited.add(new_goals_pp)
+                        new_steps = steps + [tac]
 
                         if new_state['proof_finished']:
-                            return new_proof
+                            return new_steps
 
                         # Use negative score because heapq is a min-heap
-                        next_beam.append((-score, new_state, new_goals, new_proof))
+                        next_beam.append((-new_score, new_state, new_goals, new_steps, []))
 
                     except Exception as e:
+                        if tac not in incorrect_steps:
+                            incorrect_steps.append(tac)                      
                         logging.warning(f"Invalid Tactic: {tac} {e}")
             except Exception as e:
+                print(e)
                 logging.warning(f"Tactic generation failed: {e}")
-
         if not next_beam:
             return None
 
         # Keep top `beam_size` candidates
-        beam = [(state, goals, proof) for _, state, goals, proof in heapq.nsmallest(beam_size, next_beam)]
+        beam = [(score, state, goals, steps, incorrect_steps) for score, state, goals, steps, incorrect_steps in heapq.nsmallest(beam_size, next_beam)]
 
     return None
